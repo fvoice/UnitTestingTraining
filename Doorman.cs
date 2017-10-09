3d72d62c72853b61cf0ce438f17a94ac5cdf3973
fvoice <fvoice@yandex.ru>
2017-10-09 16:46:38 +0600

    First lesson - Doorman tests

---
+++ Doorman.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassGreeting
+{
+	public class Doorman
+	{
+		private const string HELLO_FORMAT_STRING = "Hello, {0}.";
+		private const string UPPER_HELLO_FORMAT_STRING = "HELLO, {0}!";
+		private const string DEFAULT_HELLO_FORMAT_STRING = "Hello, my friend.";
+
+		public string Greet(params string[] names)
+		{
+			if (names == null) return DEFAULT_HELLO_FORMAT_STRING;
+
+			var upperCaseNames = names.Where(x => x.All(Char.IsUpper)).ToArray();
+			var usualNames = names.Except(upperCaseNames).ToArray();
+
+			var usualNamesString = ProduceSequenceOfNames(usualNames);
+			var upperCaseNamesString = ProduceSequenceOfNames(upperCaseNames);
+
+			StringBuilder result = new StringBuilder();
+			if (usualNamesString.Length > 0) result.AppendFormat(HELLO_FORMAT_STRING, usualNamesString);
+			if (upperCaseNamesString.Length > 0)
+			{
+				var upperNamesGreetingString = string.Format(UPPER_HELLO_FORMAT_STRING, upperCaseNamesString.ToUpper());
+				var appendFormatString = result.Length > 0 ? " AND {0}" : "{0}";
+				result.AppendFormat(appendFormatString, upperNamesGreetingString);
+			}
+
+			return result.ToString();
+		}
+
+		private string ProduceSequenceOfNames(string[] names)
+		{
+			if (!names.Any()) return string.Empty;
+
+			var expandedNames = ExpandComplexNames(names);
+
+			return FormatNames(expandedNames);
+		}
+
+		private static string FormatNames(string[] names)
+		{
+			int lastCommaIndex = -1;
+			StringBuilder sb = new StringBuilder();
+			for (int index = 0; index < names.Length; index++)
+			{
+				var isLast = index == names.Length - 1;
+				if (isLast)
+				{
+					lastCommaIndex = sb.Length - 2;
+				}
+				var name = names[index];
+				var format = isLast ? "{0}" : "{0}, ";
+				sb.AppendFormat(format, name);
+			}
+
+			var namesString = sb.ToString();
+
+			if (lastCommaIndex > 0)
+			{
+				namesString = namesString.Remove(lastCommaIndex, 2);
+				namesString = namesString.Insert(lastCommaIndex, " and ");
+			}
+			return namesString;
+		}
+
+		private static string[] ExpandComplexNames(string[] names)
+		{
+			var expandedNames = new List<string>();
+			foreach (var name in names)
+			{
+				if (name.StartsWith("*") && name.EndsWith("*"))
+				{
+					expandedNames.Add(name.Trim('*'));
+				}
+				else
+				{
+					expandedNames.AddRange(name.Split(',').Select(x => x.Trim()));
+				}
+			}
+			return expandedNames.ToArray();
+		}
+	}
+}
---
+++ GreetingTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ClassGreeting
+{
+	public class DoormanFixture
+	{
+		private Doorman _doorman;
+
+		[SetUp]
+		public void Setup()
+		{
+			_doorman = new Doorman();
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_given_name() //1
+		{
+			string greeting = _doorman.Greet("Bob");
+
+			greeting.Should().Be("Hello, Bob.");
+		}
+
+		[Test]
+		public void Greet_should_say_my_friend_for_null_name() //2
+		{
+			string greeting = _doorman.Greet(null);
+
+			greeting.Should().Be("Hello, my friend.");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_in_uppercase_for_uppercase_name() //3
+		{
+			string greeting = _doorman.Greet("JERRY");
+
+			greeting.Should().Be("HELLO, JERRY!");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_two_names() //4
+		{
+			string greeting = _doorman.Greet("Jerry", "Bob");
+
+			greeting.Should().Be("Hello, Jerry and Bob.");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_array_of_names() //5
+		{
+			string greeting = _doorman.Greet("Jerry", "Bob", "Mike");
+
+			greeting.Should().Be("Hello, Jerry, Bob and Mike.");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_array_of_names_and_in_UPPER_case_for_UPPER_names() //6
+		{
+			string greeting = _doorman.Greet("Jerry", "BOB", "Mike");
+
+			greeting.Should().Be("Hello, Jerry and Mike. AND HELLO, BOB!");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_array_of_names_with_string_arrays() //7
+		{
+			string greeting = _doorman.Greet("Jerry", "Bob, Mike");
+
+			greeting.Should().Be("Hello, Jerry, Bob and Mike.");
+		}
+
+		[Test]
+		public void Greet_should_say_hello_for_array_of_names_with_string_arrays_and_allow_escaping() //8
+		{
+			string greeting = _doorman.Greet("Jerry", "*Bob, Mike*");
+
+			greeting.Should().Be("Hello, Jerry and Bob, Mike.");
+		}
+	}
+}
